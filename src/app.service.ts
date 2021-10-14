b42d0704e1049770cb5517e3f7c4b6b10491e9b8
ismoria <ismoria@gmail.com>
2021-10-14 20:08:51 +0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { OrderBody, Order } from './order';
+import { Boxes, QR, Status } from './box';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private orders: Map<string, Order>;
+  private boxes: Boxes;
+
+  private static getQr(): string {
+    return new Date().getTime().toString();
+  }
+
+  private getFreeBox(): number {
+    return this.boxes.indexOf(Status.free);
+  }
+
+  constructor() {
+    this.orders = new Map<string, Order>();
+    this.boxes = new Array<Status>(100).fill(Status.free); // 100?
+  }
+
+  public saveOrder(orderBody: OrderBody): string {
+    const newOrder: Order = {
+      items: orderBody.order,
+      id: this.orders.size + 1,
+      boxId: this.getFreeBox(),
+      qr: AppService.getQr(),
+    };
+
+    this.orders.set(newOrder.qr, newOrder);
+    this.updateBox(newOrder.boxId, Status.full); // taken?
+    return newOrder.qr;
+  }
+
+  public getBoxId(qr: QR): number {
+    if (!this.orders.has(qr.qrStr)) {
+      return -1;
+    }
+    const boxId = this.orders.get(qr.qrStr).boxId;
+    this.updateBox(boxId, Status.free);
+    return boxId;
+  }
+
+  public updateBox(boxId: number, status: Status): void {
+    this.boxes[boxId] = status;
   }
 }
+
